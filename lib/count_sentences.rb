7a15ef9bc1212d0c9bdce082d847e52424261598
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-29 19:36:16 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,16 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    @sentence = "HI!"
-    self.end_with?("!")
+    @sentence = "Hi, my name is Sophie."
+    self.end_with?(".")
   end
 
   def question?
-
+    @question = "What's your name?"
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    @exclamation = "Hi, my name is Sophie!"
+    self.end_with?("!")
   end
 
   def count_sentences
